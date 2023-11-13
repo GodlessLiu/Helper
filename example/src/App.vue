5913dc891a0b6238ae6759d740f60ecf1d1905ce
Hilary liu <2788370451@qq.com>
2023-11-13 23:54:32 +0800

    init

---
+++ example/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Helper from "@helper/core"
+import {onMounted} from "vue"
+
+onMounted(() => {
+  new Helper({
+    name:"laf"
+  })
+})
+</script>
+
+<template>
+  <div>
+    main
+  </div>
+</template>
---
+++ packages/src/index.ts
@@ -0,0 +1,10 @@
+
+
+
+
+export default class Helper {
+    constructor(options){
+        console.log('Helper', options);
+        
+    }
+}
